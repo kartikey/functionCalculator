495774f349ea7367f7269b962103a347ab41e95d
studentuser <studentuser@studentusers-macbook-pro.local>
2015-10-13 16:09:03 -0700

    Initial Commit

---
+++ functionCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  functionCalculator
+//
+//  Created by studentuser on 10/13/15.
+//  Copyright © 2015 kartikey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
